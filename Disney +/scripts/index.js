267d886092c93a4a1404bb60050f1957ab8ef222
Matheus Henri França <47190319+AlixHenri@users.noreply.github.com>
2022-06-28 22:21:34 -0300

    Add files via upload

--- Disney +/scripts/index.js
+++ Disney +/scripts/index.js
@@ -1,14 +1,13 @@
 const API_KEY = 'd727d54c8332440dd1380a50e879ce22'
 const API_LANGUAGE = 'pt-br'
-const INITIAL_MOVIE = 'tt12801262'
-const LIST_MOVIE = ['tt12801262']
+const LIST_MOVIES = ['tt4520988']
 
 function getUrlMovie(movieId){
     return `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=${API_LANGUAGE}`
 }
 
 //Script para inicializar os dados do filme principal
-fetch(getUrlMovie(INITIAL_MOVIE))
+fetch(getUrlMovie(LIST_MOVIES[0]))
 .then(response => response.json())
 .then(data => {
     console.log(data)
@@ -26,6 +25,6 @@ fetch(getUrlMovie(INITIAL_MOVIE))
     rating.innerHTML = data.vote_average
     info.innerHTML = yearRelease + ' - ' + data.genres[0].name + ' - Filme'
 
-    const image = `https://image.tmdb.org/t/p/original${data.background_path}`
-    app.style.backgroundImage = `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${background}')`
+    const image = `https:image.tmdb.org/t/p/original${data.backdrop_path}`
+    app.style.backgroundImage = `linear-gradient(90.18deg, rgb(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
 })
