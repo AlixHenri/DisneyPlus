198089d7d92a7f2ddec9271d0d1293d4c6f18002
Matheus Henri França <47190319+AlixHenri@users.noreply.github.com>
2022-06-29 14:37:52 -0300

    Add files via upload

--- Disney +/scripts/index.js
+++ Disney +/scripts/index.js
@@ -1,6 +1,8 @@
 const API_KEY = 'd727d54c8332440dd1380a50e879ce22'
 const API_LANGUAGE = 'pt-br'
-const LIST_MOVIES = ['tt4520988']
+const BUTTON_PLAY = ''
+
+const LIST_MOVIES = ['tt12801262', 'tt4823776' ,'tt2096673', 'tt5109280', 'tt7146812', 'tt2948372', 'tt2953050', 'tt3521164', 'tt2380307', 'tt8097030']
 
 function getUrlMovie(movieId){
     return `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=${API_LANGUAGE}`
@@ -28,3 +30,38 @@ fetch(getUrlMovie(LIST_MOVIES[0]))
     const image = `https:image.tmdb.org/t/p/original${data.backdrop_path}`
     app.style.backgroundImage = `linear-gradient(90.18deg, rgb(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
 })
+
+function setMainMovie(movieId){
+    console.log('estou selecionando o filme', movieId)
+}
+
+const moviesList = document.getElementById('movies_list')
+
+function createButtonMovie(movieId){
+    const button = document.createElement('button')
+    button.setAttribute('onclick', `setMainMovie('${movieId}')`)
+    button.innerHTML = '<img src="assets/icon-play-button.png" alt="Icone butão play">'
+    return button
+}
+
+function createMovie(movieId){
+    fetch(getUrlMovie(movieId))
+    .then(response => response.json())
+    .then(data => {
+        const movie = document.createElement('li')
+        const genre = `<span>${data.genres[0].name}</span>`
+        const title = `<strong>${data.title}</strong>`
+        const image = `https:image.tmdb.org/t/p/original${data.backdrop_path}`
+
+        movie.innerHTML =  genre + title 
+        movie.appendChild(createButtonMovie(movieId))
+        movie.style.backgroundImage = `linear-gradient(90.18deg, rgb(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
+        moviesList.appendChild(movie)
+    })
+}
+
+function loadListMovies(){
+    LIST_MOVIES.map(createMovie)
+}
+
+loadListMovies()
